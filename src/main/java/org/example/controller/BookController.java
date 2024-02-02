bb95fadee91f07ddac8edabcafd9798ce630dc83
janath <janathhma@gmail.com>
2024-02-02 09:34:10 +0530

    complete add book method

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -3,16 +3,19 @@ package org.example.controller;
 import org.example.dto.Book;
 import org.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("./book")
 public class BookController {
-
     @Autowired
     BookService bookService;
-    public void addBook(Book book){
 
+    @PostMapping
+    public void addBook(@RequestBody Book book){
+        bookService.addBook(book);
     }
 }
--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -2,7 +2,9 @@ package org.example.service.impl;
 
 import org.example.dto.Book;
 import org.example.service.BookService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
     @Override
     public void addBook(Book book) {
